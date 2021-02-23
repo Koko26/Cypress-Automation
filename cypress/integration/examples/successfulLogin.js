ba930f114d034804d9df3e35247833f4d8ca2412
Koko26 <otueffiwatt@yahoo.com>
2021-02-23 13:36:54 +0100

    first commit

---
+++ cypress/integration/examples/successfulLogin.js
@@ -0,0 +1,42 @@
+/// <reference types= "cypress" />
+
+describe(' Regression TestSuite', function()
+{
+
+
+
+it('Validate that user can login to Twitter successfully', function() {
+
+ //Open the Twitter url
+cy.visit('http://twitter.com')
+  
+  //Click login button
+cy.get('[data-testid=loginButton] > .r-1awozwy').click()
+
+cy.wait(2000)
+//Input Twitter username16y2uox
+cy.contains('Phone').type('otueffiwatt@icloud.com')
+
+//Input Twitter password
+cy.contains('Password').type('Kokodaddy#15')
+
+//Click login button
+cy.contains('Login').click()
+
+//Assertion for successful login
+
+if(cy.get('.r-18kxxzh > .css-4rbku5 > .r-1twgtwe').should('be.visible'))
+{
+  cy.log('Test Passed')
+} 
+
+
+else {
+  cy.log('Test Failed')
+}
+
+})
+
+
+
+})
